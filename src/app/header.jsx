dfea1595c45022990f5775bbecb626e68660060c
opu-rayhan <apuhot168@gmail.com>
2023-10-02 23:23:13 +0600

    agin

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function header() {
   return (
-    <>
+    <div>
       <div className=" bg-slate-50 w-full h-auto  flex ">
         <Logo />
         <div className=" w-auto m-auto bg-slate-200 ">
@@ -16,7 +16,7 @@ export default function header() {
           </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 

f049d5367edac949ec41083057ee50ef29674348
opu-rayhan <apuhot168@gmail.com>
2023-10-02 23:08:30 +0600

    customice header component

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -2,30 +2,32 @@
 import Logo from "@/components/Logo";
 import Link from "next/link";
 
-const header = () => {
+// <li className=" mr-5 ">
+//   <Link href="./home">Home</Link>
+// </li>
+
+export default function header() {
   return (
     <>
       <div className=" bg-slate-50 w-full h-auto  flex ">
         <Logo />
-        <div className=" w-1/4 m-auto bg-slate-200 ">
+        <div className=" w-auto m-auto bg-slate-200 ">
           <ul className=" flex p-5 justify-center">
-            <li className=" mr-5 ">
-              <Link href="./home">Home</Link>
-            </li>
-            <li className=" mr-5 hover:font-bold hover:text-red-500 ">
-              <Link href="./login">Login</Link>
-            </li>
-            <li className=" mr-5 hover:font-bold hover:text-red-500 ">
-              <Link href="./service">Service</Link>
-            </li>
-            <li className=" mr-5 hover:font-bold hover:text-red-500 ">
-              <Link href="./menu">Menu</Link>
-            </li>
+            <Nav name="Home" path="./home" />
+            <Nav name="Login" path="./login" />
+            <Nav name="Service" path="./service" />
+            <Nav name="Menu" path="./menu" />
           </ul>
         </div>
       </div>
     </>
   );
-};
+}
 
-export default header;
+const Nav = (props) => {
+  return (
+    <li className=" mr-5 hover:font-bold hover:text-red-500 ">
+      <Link href={props.path}>{props.name}</Link>
+    </li>
+  );
+};

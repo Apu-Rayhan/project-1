291c039d889c02921a814dd3074db3f98b195946
opu-rayhan <apuhot168@gmail.com>
2023-10-07 02:34:16 +0600

    ggggg

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -1,7 +1,15 @@
+import SideBar from "@/components/SideBar";
 import React from "react";
 
 const page = () => {
-  return <div> Menu page</div>;
+  return (
+    <div className="">
+      <div className="">
+        <div> Menu page</div>
+      </div>
+      <SideBar />
+    </div>
+  );
 };
 
 export default page;
